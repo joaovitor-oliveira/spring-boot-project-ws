4311e7d1a141835a4de9b2801c5a48174f772d71
João Vitor S. Oliveira <j.vitor_oliveira1@hotmail.com>
2022-09-24 19:53:03 -0300

    feat: h2 database, test profile and jpa

--- src/main/java/com/joaovitor/springstudy/SpringstudyApplication.java
+++ src/main/java/com/joaovitor/springstudy/SpringstudyApplication.java
@@ -1,4 +1,4 @@
-package com.joaovitor.ws;
+package com.joaovitor.springstudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/joaovitor/springstudy/resources/UserResource.java
+++ src/main/java/com/joaovitor/springstudy/resources/UserResource.java
@@ -1,11 +1,11 @@
-package com.joaovitor.ws.resources;
+package com.joaovitor.springstudy.resources;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.joaovitor.ws.entities.User;
+import com.joaovitor.springstudy.entities.User;
 
 @RestController
 @RequestMapping(value = "/users")
