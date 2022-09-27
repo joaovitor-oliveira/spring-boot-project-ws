df60364ed8620d2cd41f3456a940929ac9ffa780
João Vitor S. Oliveira <j.vitor_oliveira1@hotmail.com>
2022-09-26 23:21:46 -0300

    feat: product

--- src/main/java/com/joaovitor/springstudy/config/TestConfig.java
+++ src/main/java/com/joaovitor/springstudy/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.joaovitor.springstudy.entities.Category;
 import com.joaovitor.springstudy.entities.Order;
+import com.joaovitor.springstudy.entities.Product;
 import com.joaovitor.springstudy.entities.User;
 import com.joaovitor.springstudy.enums.OrderStatus;
 import com.joaovitor.springstudy.repositories.CategoryRepository;
 import com.joaovitor.springstudy.repositories.OrderRepository;
+import com.joaovitor.springstudy.repositories.ProductRepository;
 import com.joaovitor.springstudy.repositories.UserRepository;
 
 @Configuration
@@ -29,6 +31,9 @@ public class TestConfig implements CommandLineRunner {
 	@Autowired
 	private CategoryRepository categoryRepository;
 	
+	@Autowired
+	private ProductRepository productRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
@@ -43,9 +48,15 @@ public class TestConfig implements CommandLineRunner {
 		Category cat2 = new Category(null, "Books");
 		Category cat3 = new Category(null, "Computers");
 		
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, ""); new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p2 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p3 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p4 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4));
 	}
 
 }
