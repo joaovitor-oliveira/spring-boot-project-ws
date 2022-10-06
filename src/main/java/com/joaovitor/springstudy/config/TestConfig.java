89de1ff8ce9e1c5a0538ebe2ee85d0d3092953e1
João Vitor S. Oliveira <j.vitor_oliveira1@hotmail.com>
2022-10-06 00:05:45 -0300

    feat: Payment and one-to-one association

--- src/main/java/com/joaovitor/springstudy/config/TestConfig.java
+++ src/main/java/com/joaovitor/springstudy/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.joaovitor.springstudy.entities.Category;
 import com.joaovitor.springstudy.entities.Order;
 import com.joaovitor.springstudy.entities.OrderItem;
+import com.joaovitor.springstudy.entities.Payment;
 import com.joaovitor.springstudy.entities.Product;
 import com.joaovitor.springstudy.entities.User;
 import com.joaovitor.springstudy.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
---
+++ src/main/java/com/joaovitor/springstudy/entities/Payment.java
@@ -0,0 +1,81 @@
+package com.joaovitor.springstudy.entities;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant date;
+	
+	@OneToOne
+	@MapsId
+	private Order order;
+	
+	public Payment() {
+		
+	}
+
+	public Payment(Long id, Instant date, Order order) {
+		this.id = id;
+		this.date = date;
+		this.order = order;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Payment other = (Payment) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
